a0d2652ebe0f139e1554fb6dd1956d3ddc0777a1
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-02-05 19:38:10 +0200

    Remove settings.py

--- task_1/main.py
+++ task_1/main.py
@@ -4,9 +4,11 @@ import aiosqlite
 
 from task_1 import settings
 
+DB_FILE = "posts.db"
+
 
 async def setup_db() -> None:
-    async with aiosqlite.connect(settings.DB_FILE) as db:
+    async with aiosqlite.connect(DB_FILE) as db:
         await db.execute(
             """
             CREATE TABLE IF NOT EXISTS posts (
