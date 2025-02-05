a13c5dec513e37d4fe718e0bda47498b9d5dadcb
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-02-05 20:29:14 +0200

    Fix script by adding two queues (for file and for db)

--- task_1/main.py
+++ task_1/main.py
@@ -38,7 +38,8 @@ async def setup_db() -> None:
 async def fetch_post(
     session: aiohttp.ClientSession,
     post_id: int,
-    queue: asyncio.Queue,
+    db_queue: asyncio.Queue,
+    csv_queue: asyncio.Queue,
 ):
     try:
         async with session.get(f"{API_URL}/{post_id}/") as response:
@@ -46,19 +47,29 @@ async def fetch_post(
             if response.status == 200:
                 data = await response.json()
                 post = (data["id"], data["userId"], data["title"], data["body"])
-                await queue.put(post)
+                await db_queue.put(post)
+                await csv_queue.put(post)
             else:
                 logging.error(f"Error {response.status} fetching post {post_id}")
     except Exception as e:
         logging.error(f"Exception fetching post {post_id}: {e}")
 
 
-async def fetch_all_posts() -> asyncio.Queue:
-    queue = asyncio.Queue()
+async def fetch_all_posts() -> tuple[asyncio.Queue, asyncio.Queue]:
+    db_queue = asyncio.Queue()
+    csv_queue = asyncio.Queue()
     async with aiohttp.ClientSession() as session:
-        tasks = [fetch_post(session, i, queue) for i in range(1, 101)]
+        tasks = [
+            fetch_post(
+                session=session,
+                post_id=i,
+                db_queue=db_queue,
+                csv_queue=csv_queue,
+            )
+            for i in range(1, 101)
+        ]
         await asyncio.gather(*tasks)
-    return queue
+    return db_queue, csv_queue
 
 
 async def save_to_db(queue: asyncio.Queue) -> None:
@@ -71,11 +82,9 @@ async def save_to_db(queue: asyncio.Queue) -> None:
 
 
 async def save_to_csv(queue: asyncio.Queue) -> None:
-    async with aiofiles.open(CSV_FILE, "w", encoding="utf-8") as csv_file:
-        fieldnames = ["id", "user_id", "title", "body"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-
+    async with aiofiles.open(CSV_FILE, "a", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        await writer.writerow(["id", "user_id", "title", "body"])
         while not queue.empty():
             post = await queue.get()
             await writer.writerow(post)
@@ -84,6 +93,8 @@ async def save_to_csv(queue: asyncio.Queue) -> None:
 
 async def main() -> None:
     await setup_db()
+    db_queue, csv_queue = await fetch_all_posts()
+    await asyncio.gather(save_to_db(db_queue), save_to_csv(csv_queue))
 
 
 if __name__ == "__main__":
