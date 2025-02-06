01d218fbfd1d3b437e1ce65f296cec663eafc03e
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-02-06 16:13:39 +0200

    Fix logging

--- task_3/log_analyzer.py
+++ task_3/log_analyzer.py
@@ -1,14 +1,14 @@
 import collections
 import logging
 import re
-
+import sys
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
         logging.FileHandler(filename="log_analyzer.log", mode="a"),
-        logging.StreamHandler(),
+        logging.StreamHandler(stream=sys.stdout),
     ],
 )
 
