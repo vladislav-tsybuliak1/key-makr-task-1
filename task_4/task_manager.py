3cc07f34fa1cb89f018e15f4ce4f1ad9e9c180d9
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-02-06 17:30:29 +0200

    Add db setup

--- task_4/task_manager.py
+++ task_4/task_manager.py
@@ -1,11 +1,35 @@
 import logging
+import sqlite3
 import sys
+
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler(filename="log_analyzer.log", mode="a"),
+        logging.FileHandler(filename="task_manager.log", mode="a"),
         logging.StreamHandler(stream=sys.stdout),
     ],
 )
+
+
+DB_FILE = "tasks.db"
+
+
+def setup_db() -> None:
+    with sqlite3.connect(DB_FILE) as db:
+        db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title VARCHAR(255) NOT NULL,
+                description TEXT,
+                due_date DATETIME,
+                status TEXT CHECK(status IN ('pending', 'in_progress', 'completed')) NOT NULL DEFAULT 'pending'
+            )
+            """
+        )
+
+
+if __name__ == "__main__":
+    setup_db()
