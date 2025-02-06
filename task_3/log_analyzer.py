dc9b1a856ea533fd764702ab963c315feb0af266
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-02-06 15:43:58 +0200

    Add parse log function

--- task_3/log_analyzer.py
+++ task_3/log_analyzer.py
@@ -3,3 +3,11 @@ import re
 LOG_PATTERN = re.compile(
     r'(?P<ip>\d+\.\d+\.\d+\.\d+) - - \[(?P<datetime>.*?)] "(?P<method>\w+) (?P<url>.*?) (?P<protocol>HTTP/\d\.\d)" (?P<status>\d+) (?P<size>\d+)'
 )
+
+
+def parse_log(file_path):
+    with open(file_path, "r") as file:
+        for line in file:
+            match = LOG_PATTERN.match(line)
+            if match:
+                yield match.groupdict()
