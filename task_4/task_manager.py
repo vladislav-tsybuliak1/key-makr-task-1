d8b8426be804eb526c1acd9973cf552dc096964f
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-02-07 12:27:32 +0200

    Add function for adding a task

--- task_4/task_manager.py
+++ task_4/task_manager.py
@@ -1,6 +1,7 @@
 import logging
 import sqlite3
 import sys
+from datetime import datetime
 
 
 logging.basicConfig(
@@ -16,6 +17,13 @@ logging.basicConfig(
 DB_FILE = "tasks.db"
 
 
+
+def adapt_datetime(dt):
+    return dt.isoformat()
+
+sqlite3.register_adapter(datetime, adapt_datetime)
+
+
 def setup_db() -> None:
     with sqlite3.connect(DB_FILE) as db:
         db.execute(
@@ -24,12 +32,21 @@ def setup_db() -> None:
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title VARCHAR(255) NOT NULL,
                 description TEXT,
-                due_date DATETIME,
+                due_date DATETIME NOT NULL,
                 status TEXT CHECK(status IN ('pending', 'in_progress', 'completed')) NOT NULL DEFAULT 'pending'
             )
             """
         )
 
 
+def add_task(title: str, due_date: datetime, description: str = None) -> None:
+    with sqlite3.connect(DB_FILE) as db:
+        db.execute(
+            "INSERT INTO tasks (title, description, due_date) VALUES (?, ?, ?)",
+            (title, description, due_date),
+        )
+
+
 if __name__ == "__main__":
     setup_db()
+    add_task(title="Title", due_date=datetime(2023, 12, 31, 12))
